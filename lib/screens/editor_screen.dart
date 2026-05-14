dce3370347d05e8fe0e876b77e62bba2138274cd
keitac296-dotcom <keitac296@gmail.com>
2026-05-14 10:55:58 +0000

    Update editor_screen.dart

--- lib/screens/editor_screen.dart
+++ lib/screens/editor_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/video_picker.dart';
+import '../widgets/image_picker_widget.dart';
 import 'dart:io';
 
 class EditorScreen extends StatefulWidget {
@@ -9,10 +10,17 @@ class EditorScreen extends StatefulWidget {
 
 class _EditorScreenState extends State<EditorScreen> {
   File? _selectedVideo;
+  File? _selectedImage;
 
   void _onVideoSelected(File video) {
     setState(() {
       _selectedVideo = video;
+    });
+  }
+
+  void _onImageSelected(File image) {
+    setState(() {
+      _selectedImage = image;
     });
   }
 
@@ -21,13 +29,19 @@ class _EditorScreenState extends State<EditorScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('Éditeur')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            VideoPickerWidget(onVideoSelected: _onVideoSelected),
-            _selectedVideo != null ? Text('Fichier : ${_selectedVideo!.path}') : Container(),
-            // Ici plus tard : boutons pour couper, ajouter texte/stickers
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              VideoPickerWidget(onVideoSelected: _onVideoSelected),
+              _selectedVideo != null ? Text('Vidéo : ${_selectedVideo!.path}') : Container(),
+              SizedBox(height: 20),
+              ImagePickerWidget(onImageSelected: _onImageSelected),
+              _selectedImage != null
+                  ? Text('Image sélectionnée : ${_selectedImage!.path}')
+                  : Container(),
+            ],
+          ),
         ),
       ),
     );
