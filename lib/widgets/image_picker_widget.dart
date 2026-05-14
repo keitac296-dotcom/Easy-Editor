e10745659500a688d2fa9c1c13c28246721727fa
keitac296-dotcom <keitac296@gmail.com>
2026-05-14 10:54:07 +0000

    Create lib/widgets/image_picker_widget.dart

---
+++ lib/widgets/image_picker_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class ImagePickerWidget extends StatefulWidget {
+  final Function(File) onImageSelected;
+
+  ImagePickerWidget({required this.onImageSelected});
+
+  @override
+  _ImagePickerWidgetState createState() => _ImagePickerWidgetState();
+}
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget> {
+  File? _imageFile;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+      widget.onImageSelected(_imageFile!);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: _pickImage,
+          child: Text('Sélectionner Image'),
+        ),
+        _imageFile != null
+            ? Image.file(_imageFile!, width: 200, height: 200, fit: BoxFit.cover)
+            : Container(),
+      ],
+    );
+  }
+}
