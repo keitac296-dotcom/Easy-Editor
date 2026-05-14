e3925e568f4461f754d2cff62633496f9031a353
keitac296-dotcom <keitac296@gmail.com>
2026-05-14 10:50:42 +0000

    Update editor_screen.dart

--- lib/screens/editor_screen.dart
+++ lib/screens/editor_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import '../widgets/video_picker.dart';
+import 'dart:io';
 
-class EditorScreen extends StatelessWidget {
+class EditorScreen extends StatefulWidget {
+  @override
+  _EditorScreenState createState() => _EditorScreenState();
+}
+
+class _EditorScreenState extends State<EditorScreen> {
+  File? _selectedVideo;
+
+  void _onVideoSelected(File video) {
+    setState(() {
+      _selectedVideo = video;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Éditeur')),
       body: Center(
-        child: Text('Ici tu pourras couper, ajouter texte et stickers'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            VideoPickerWidget(onVideoSelected: _onVideoSelected),
+            _selectedVideo != null ? Text('Fichier : ${_selectedVideo!.path}') : Container(),
+            // Ici plus tard : boutons pour couper, ajouter texte/stickers
+          ],
+        ),
       ),
     );
   }
